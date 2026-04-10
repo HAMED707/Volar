79f681ce129d1244106577615090130b05c2e1d4
Abdelrahman Hamed <h70abdelrahmanelsayed@gmail.com>
2026-04-10 20:43:39 +0200

    Start building the template of the products app

--- apps/products/models.py
+++ apps/products/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     price       = models.DecimalField(max_digits=10,decimal_places=2)
     stock       = models.PositiveIntegerField()
     slug        = models.SlugField(unique=True)
+    image       = models.ImageField(upload_to="products/", blank=True, null=True)
     is_active   = models.BooleanField(default=True)
     created_at  = models.DateTimeField(auto_now_add=True)
 
--- backend/settings/base.py
+++ backend/settings/base.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                "apps.products.context_processors.nav_categories",
+                
             ],
         },
     },
@@ -137,3 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_URL= "/media/"
+MEDIA_ROOT= BASE_DIR / "media"
