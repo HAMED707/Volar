bdac33e5ff9d741028de9d81c2db6338d5303f09
Abdelrahman Hamed <h70abdelrahmanelsayed@gmail.com>
2026-04-14 07:12:24 +0200

    building the search view

--- apps/products/views.py
+++ apps/products/views.py
@@ -17,3 +17,15 @@ class ProductDetailView(DetailView):
     model = Product
     template_name="products/product_detail.html"
     context_object_name="product"
+
+class SearchView(ListView):
+    model=Product
+    template_name="products/search_results.html"
+    context_object_name="products"
+
+    def get_queryset(self):
+        
+        query=self.request.GET.get("q")
+        if not query:
+            return Product.objects.none()
+        return Product.objects.filter(name__icontains=query)
